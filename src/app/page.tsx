996828b7e62c3bb3e0055fbd63fc8b1f9a5ffc43
HenriqueCosta05 <Henriquebenedictocostadev@gmail.com>
2024-10-24 10:59:06 -0300

    feat: navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="w-full h-screen bg-slate-300 flex justify-center items-center flex-col">
+    <main className="w-full h-screen flex justify-center items-center flex-col">
       <h1 className="text-5xl font-black">Hello World</h1>
     </main>
   );
